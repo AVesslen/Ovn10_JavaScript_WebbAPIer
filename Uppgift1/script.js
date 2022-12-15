6c1f69e2cfc960331e9b95c7281b4bb00e1ecf69
Anna Vesslén <anna_ananaz@hotmail.com>
2022-12-15 11:06:21 +0100

    cleaned the code

--- Uppgift1/script.js
+++ Uppgift1/script.js
@@ -1,32 +1,22 @@
 let input=document.querySelector('#input');
 let button=document.querySelector('#button');
 let output=document.querySelector('#output');
-let outputName=document.querySelector('#outputName');
-
-
 
 function getApi(){    
     
     let firstUri='https://www.swapi.tech/api/people/?name=';
-    let fullUri=`${firstUri}${input.value}`;
-    //console.log(input.value);
-    //console.log(fullUri);
+    let fullUri=`${firstUri}${input.value}`;   
 
     fetch(fullUri)
         .then(res => res.json())
         .then(data =>{
                      
-           let properties=data.result[0].properties;
-           console.log(data);
-           console.log(properties);
-           let propString=`${properties.name}\nHeight: ${properties.height}\nMass: ${properties.mass}\nGender: ${properties.gender}\nHair color: ${properties.hair_color}`;
-           
-           //outputName.innerHTML=`Info about ${properties.name}:`;
-           //output.innerHTML=`${properties.name}`;
+           let properties=data.result[0].properties;          
+           let propString=`${properties.name}\n\nHeight: ${properties.height}\nMass: ${properties.mass}\nGender: ${properties.gender}\nHair color: ${properties.hair_color}\nEye color: ${properties.eye_color}`;
+                     
            output.innerHTML=propString;            
         })
         .catch(err => console.log(err))
 }
-
 
 button.addEventListener('click', getApi);
